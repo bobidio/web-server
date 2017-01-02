2a4a63f70feac1cf60fbef52c48276df38f67bf6
bobidio <bobidio@gmail.com>
2017-01-03 00:42:28 +0200

    cozy

--- server.js
+++ server.js
@@ -2,18 +2,8 @@ var express = require('express');
 var app = express();
 
 var PORT = 3000
+var middleware = require('./middleware.js');
 
-var middleware = {
-	requireAuthentication: function(req, res, next){
-		console.log('private root hit');
-		next();
-
-	},
-	logger: function (req, res, next){
-		console.log(new Date().toString() +' ' +req.method + ' ' + req.originalUrl);
-		next();
-	}
-};
 app.use(middleware.logger);
 app.get('/about', middleware.requireAuthentication, function (req, res){
 	res.send('hello moron!!!')
